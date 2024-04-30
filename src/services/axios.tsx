2bde64967fdbed6768ce9d20c26304791f89a058
valclassdevelop <developervalclass.com>
2024-04-30 11:07:02 +0700

    web ecoDashboard

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import store from "../redux/store";
 
 const api = axios.create({
-    // baseURL: "https://be-econusantara.vercel.app",
-    baseURL: "http://localhost:3600",
+    // baseURL: "http://localhost:3600",
+    baseURL: "https://be-econusantara.vercel.app",
     headers: {
         "Content-Type": "application/json",
     },
